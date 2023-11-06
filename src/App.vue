9c92bcd16b97f21367c304fe219232fdac03be28
Prain Lopez <qym200042@gmail.com>
2023-11-06 15:52:32 +0800

    1106

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { RouterView } from 'vue-router'
-import TodoHeader from './components/TodoHeader.vue';
+import TodoHeader from './components/TodoHeader.vue'
 </script>
 
 <template>
@@ -12,7 +12,7 @@ import TodoHeader from './components/TodoHeader.vue';
 @import url('https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;700&display=swap');
 
 * {
-  font-family: "Space Grotesk", sans-serif;
+  font-family: 'Space Grotesk', sans-serif;
   margin: 0;
   padding: 0;
   box-sizing: border-box;
