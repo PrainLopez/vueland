9879e41a0a68a6c06fd19c57aa5313023aa007ad
Prain Lopez <qym200042@gmail.com>
2023-11-07 17:20:29 +0800

    下班

--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue'
 import { uid } from 'uid'
 import TodoCreator from '../components/TodoCreator.vue'
+import TodoItem from '../components/TodoItem.vue'
 
 const todoList = ref([])
 
@@ -19,6 +20,9 @@ const addTodo = (todo) => {
   <main>
     <h1>Create Todo</h1>
     <TodoCreator @create-todo="addTodo" />
+    <ul>
+      <TodoItem v-for="todo in todoList" :todo="todo" />
+    </ul>
   </main>
 </template>
 
