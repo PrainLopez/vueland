4c98f7cd8f520db0bd5474ae78f42149240be0ad
Prain Lopez <qym200042@gmail.com>
2023-11-16 17:23:18 +0800

    下班

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { Icon } from '@iconify/vue'
+
 const props = defineProps({
   todo: {
     type: Object,
@@ -8,7 +10,20 @@ const props = defineProps({
 </script>
 
 <template>
-  <div>Todo Item</div>
+  <li>
+    <input type="checkbox" :checked="todo.isCompleted" />
+    <div class="todo">
+      <input type="text" :value="todo.todo" />
+      <span>
+        {{ todo.todo }}
+      </span>
+    </div>
+    <div class="todo-actions">
+      <Icon icon="ph:check-circle" class="icon" color="#41b080" width="22" />
+      <Icon icon="ph:pencil-fill" class="icon" color="#41b080" width="22" />
+      <Icon icon="ph:trash" class="icon" color="#f95e5e" width="22" />
+    </div>
+  </li>
 </template>
 
 <style lang="scss" scoped></style>
