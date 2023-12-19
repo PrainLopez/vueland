1086fa802e93b116362f91829d704aa413f92185
Prain Lopez <qym200042@gmail.com>
2023-12-19 17:04:58 +0800

    👏👏👏tutorial finished!

--- src/router/index.js
+++ src/router/index.js
@@ -7,15 +7,20 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: TodosView
+      component: TodosView // This component has been imported, and will force load everytime
     },
     {
       path: '/about',
       name: 'about',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
+      // which is **lazy-loaded** when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/test',
+      name: 'test',
+      component: () => import('../views/TestingView.vue')
     }
   ]
 })
---
+++ src/views/TestingView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h3>This is a test for the new route</h3>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
