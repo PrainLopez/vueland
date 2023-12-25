402075804407a540a8c881160e703c9ec7c542ff
Prain Lopez <qym200042@gmail.com>
2023-12-25 11:00:13 +0800

    午休

--- src/stores/todoList.js
+++ src/stores/todoList.js
@@ -3,6 +3,14 @@ import { defineStore } from 'pinia'
 export const useTodoListStore = defineStore('todoList', {
   state: () => [],
   actions: {
+    addTodo(todo) {
+      todoList.value.push({
+        id: uid(),
+        todo,
+        isCompleted: null,
+        isEditing: null
+      })
+    },
     toggleComplete(index) {},
     toggleEdit(index) {},
     deleteTodo(todoId) {}
